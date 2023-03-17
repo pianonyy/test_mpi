737b32e76639ada8b8d8517634afdbd8446d2faf
pianonyy <48867176+pianonyy@users.noreply.github.com>
2023-03-17 23:15:50 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+struct Point2D {
+    double x;
+    double y;
+};
+
+std::vector<Point2D> loadPointsFromFile(const std::string& filePath) {
+    std::vector<Point2D> points;
+
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filePath << std::endl;
+        return points;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string token;
+
+        Point2D point;
+        std::getline(ss, token, ',');
+        point.x = std::stod(token);
+
+        std::getline(ss, token, ',');
+        point.y = std::stod(token);
+
+        points.push_back(point);
+    }
+
+    file.close();
+    return points;
+}
+
+int main() {
+    std::vector<Point2D> points = loadPointsFromFile("points.csv");
+
+    for (const auto& point : points) {
+        std::cout << "x: " << point.x << ", y: " << point.y << std::endl;
+    }
+
+    return 0;
+}
