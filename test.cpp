24030069f41591fc8e2e5ef87ad3a47c0f322017
pianonyy <48867176+pianonyy@users.noreply.github.com>
2023-03-17 23:35:59 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 
 struct Point2D {
@@ -7,7 +8,7 @@ struct Point2D {
     double y;
 };
 
-std::vector<Point2D> loadPointsFromFile(const std::string& filePath) {
+std::vector<Point2D> loadPointsFromCSV(const std::string& filePath) {
     std::vector<Point2D> points;
 
     std::ifstream file(filePath);
@@ -16,9 +17,18 @@ std::vector<Point2D> loadPointsFromFile(const std::string& filePath) {
         return points;
     }
 
-    double x, y;
-    while (file >> x >> y) {
-        Point2D point { x, y };
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string token;
+
+        Point2D point;
+        std::getline(ss, token, ',');
+        point.x = std::stod(token);
+
+        std::getline(ss, token, ',');
+        point.y = std::stod(token);
+
         points.push_back(point);
     }
 
@@ -27,7 +37,7 @@ std::vector<Point2D> loadPointsFromFile(const std::string& filePath) {
 }
 
 int main() {
-    std::vector<Point2D> points = loadPointsFromFile("points.csv");
+    std::vector<Point2D> points = loadPointsFromCSV("points.csv");
 
     for (const auto& point : points) {
         std::cout << "x: " << point.x << ", y: " << point.y << std::endl;
